125c1ac14740a06b68bddb42fc0a9739fa1911fc
mamariscal <mamariscal@csumb.edu>
2019-06-29 18:04:58 -0400

    update non php stuff

--- index.php
+++ index.php
@@ -1,13 +1,23 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
+  
+</html>
 <head>
   <meta charset="UTF-8">
   <title>Document</title>
 </head>
 <body>
   
+    </header>
+  <h1>
+    Mark Mariscal
+  </h1>
+    <header>
+  
+  </header>
   <h1>
     
+    <hr>
     This is my first web page!
   
   </h1>
